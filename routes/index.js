b0d71e00e0a12b91b4747c8362d8472eb607e6c9
Sopha Boss <sboss@ucsd.edu>
2021-02-03 00:32:51 -0800

    each loop

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,9 @@ var data = require('../data.json');
 
 exports.view = function(request, response){
 	console.log(data);
-	response.render('index');
+	response.render('index', data);
+//		"name": "friendName",
+//		"description": "friendDescription",
+//		"imageURL": "friendUrl"
+//	});
 };
